7860b28abe52c2d38919e32abd4f8306e4dcbbcc
Arthur Laranjeira <98919700+arthurlaranjeira@users.noreply.github.com>
2024-09-04 17:08:54 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,12 @@
+let dados = [
+    {
+        titulo: "Função",
+        descricao: "Bloco de código reutilizável que executa uma tarefa específica.",
+    },
+    {
+        titulo: "Classe",
+        descricao: "Modelo para criar objetos, com atributos (características) e métodos (ações)",    },
+    {
+        titulo: "Variável",
+        descricao: "Contêiner que armazena dados, como números, textos ou valores booleanos (verdadeiro ou falso).",    }
+];
